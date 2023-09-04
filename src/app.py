ae1ed12c5acb449bbfb077cc5f11cf137983cc0d
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-04 14:42:34 -0500

    uploading project

---
+++ src/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+st.title("Generador de imágenes con DALL-E")
+
+with st.form("images_form"):
+  text = st.text_input("Texto para generar la imagen")
+  num_images = st.number_input("Número de imágenes a generar", min_value=1, max_value=10, value=1)
+  image_size = st.selectbox("Tamaño de la imagen", ["256x256", "512x512", "1024x1024"], index=0)
+  submit_button = st.form_submit_button(label="Generar imágenes")
+
+if submit_button:
+  st.write("Generando imágenes...")
+  response = openai.Image.create(
+    prompt=text,
+    n=num_images,
+    size=image_size
+  )
+
+  for i in range(num_images):
+    url = response.data[i].url
+    st.image(url, caption=f"Imagen {i+1}", use_column_width=True)
